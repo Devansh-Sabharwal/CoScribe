07727d0ee7fdf5eee34527d1e9c51194375f40ed
Devansh Sabharwal <sabharwaldevansh04@gmail.com>
2025-03-10 22:53:27 +0530

    meta fixed

--- apps/excalidraw-frontend/app/layout.tsx
+++ apps/excalidraw-frontend/app/layout.tsx
@@ -22,7 +22,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Excalidraw",
+  title: "CoScribe",
   description: "Create and Collaborate",
   icons: [
     {
